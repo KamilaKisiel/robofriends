15d14c81f8d74f1e8d0bc3a6236ab6bdb24a32e2
KamilaKisiel <kisiel.kamila@gmail.com>
2018-06-13 19:47:41 +0200

    basic styles added to cards with tychyons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(
+            <div>
+                <Card />
+                <Card />
+                <Card />
+            </div>,
+document.getElementById('root'));
 registerServiceWorker();
