491ac5b3f62c429aa5779accf278553795ba9561
KamilaKisiel <kisiel.kamila@gmail.com>
2018-06-13 19:20:21 +0200

    main card component created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(<Card />, document.getElementById('root'));
 registerServiceWorker();
