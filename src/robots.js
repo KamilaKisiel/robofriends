f20b11b69a271a63741f509764ff0bf17cf11127
KamilaKisiel <kisiel.kamila@gmail.com>
2018-06-13 20:18:38 +0200

    content source file added

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Archie Andrews',
+        username: 'Arch',
+        email: 'archie@riverdale.com'
+    },
+    {
+        id: 2,
+        name: 'Veronica Lodge',
+        username: 'Ronnie',
+        email: 'ronnie@princess.com'
+    },
+    {
+        id: 3,
+        name: 'Jughead Jones',
+        username: 'Jug',
+        email: 'juggie@eating.com'
+    },
+    {
+        id: 4,
+        name: 'Betty Cooper',
+        username: 'Betty',
+        email: 'betty@goodgirl.com'
+    }
+]
