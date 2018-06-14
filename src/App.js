ffbdcd18088ca80a5924f9e70359c4bfdf2e27b4
KamilaKisiel <kisiel.kamila@gmail.com>
2018-06-14 21:53:22 +0200

    filtering robots on search change

--- src/App.js
+++ src/App.js
@@ -5,12 +5,28 @@ import {robots} from './robots';
 
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().
+            includes(this.state.searchfield.toLowerCase());
+        })
+        console.log(filteredRobots);
+    }
+
     render() {
     return (
         <div className='tc'>
         <h1>RoboFriends</h1>
-        <SearchBox></SearchBox>
-        <Cardlist robots={ robots }/>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Cardlist robots={ this.state.robots }/>
         </div>
     );
  }
