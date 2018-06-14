a240d7a423ef65996a529d5d9d349dee7114fc40
KamilaKisiel <kisiel.kamila@gmail.com>
2018-06-14 20:26:41 +0200

    created main app component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cardlist from './Cardlist';
+import {robots} from './robots';
+
+
+const App = () => {
+    return (
+        <Cardlist robots={ robots }/>
+    );
+}
+
+export default App;
