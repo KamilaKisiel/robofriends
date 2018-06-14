6201755df256c052d5776a7288faebdc7de2ac23
KamilaKisiel <kisiel.kamila@gmail.com>
2018-06-14 20:32:41 +0200

    created search box component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Cardlist from './Cardlist';
+import SearchBox from './SearchBox'
 import {robots} from './robots';
 
 
 const App = () => {
     return (
+        <div>
+        <h1>Robofriends</h1>
+        <SearchBox></SearchBox>
         <Cardlist robots={ robots }/>
+        </div>
     );
 }
 
