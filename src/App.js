5917b3e53d7141b2e1e75492e9a0c440fec493d8
KamilaKisiel <kisiel.kamila@gmail.com>
2018-06-15 21:59:48 +0200

    a loading solution when api is to slow

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ class App extends Component {
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         fetch('http://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({robots: users}))
+            .then(response => response.json())
+            .then(users => {this.setState({ robots: users })})
     }
 
     onSearchChange = (event) => {
@@ -27,15 +27,18 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        
-    return (
-        <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <Cardlist robots={ filteredRobots }/>
-        </div>
-    );
- }
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Cardlist robots={filteredRobots} />
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
