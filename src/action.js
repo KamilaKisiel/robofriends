0700363041eaaa9051b37f2e3400db5e31ae00f3
KamilaKisiel <kisiel.kamila@gmail.com>
2018-06-24 13:50:19 +0200

    checking how redux works by loggin it into console

--- src/action.js
+++ src/action.js
@@ -1,7 +1,10 @@
 import { CHANGE_SEARCH_FIELD } from './constants.js';
 
 
-export const setSearchField = (text) => ({
+export const setSearchField = (text) => {
+    console.log(text);
+    return {
 type: CHANGE_SEARCH_FIELD,
 payload: text
-})
+}
+}
