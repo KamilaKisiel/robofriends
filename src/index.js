367425b4c3806e18bb20f14b0bbe4671ab471df1
KamilaKisiel <kisiel.kamila@gmail.com>
2018-09-09 18:58:38 +0200

    turn off service workers

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
 import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
@@ -19,4 +18,3 @@ ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>, document.getElementById('root'));
-registerServiceWorker();
